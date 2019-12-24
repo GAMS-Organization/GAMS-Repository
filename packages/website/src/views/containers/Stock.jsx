2ad3a44a6fb4380458dc4db15a0d24cf5a36689d
javi <javisicardi94@gmail.com>
2019-12-24 11:45:55 -0300

    Table_Stock_InProcess2

--- packages/website/src/views/containers/Stock.jsx
+++ packages/website/src/views/containers/Stock.jsx
@@ -3,51 +3,52 @@ import PropTypes from 'prop-types';
 // @material-ui/core components
 import withStyles from '@material-ui/core/styles/withStyles';
 
-import TableStockSection from '../sections/Stock/TableStockSection'
+import TableStockSection from '../sections/Stock/TableStockSection';
+import LoadPurchase from '../sections/Stock/LoadPurchase';
 
 const styles = {
-    cardCategoryWhite: {
-      color: 'rgba(255,255,255,.62)',
-      margin: '0',
-      fontSize: '14px',
-      marginTop: '0',
-      marginBottom: '0',
-    },
-    cardTitleWhite: {
-      color: '#FFFFFF',
-      marginTop: '0px',
-      minHeight: 'auto',
-      fontWeight: '300',
-      fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
-      marginBottom: '3px',
-      textDecoration: 'none',
-    },
-  };
+  cardCategoryWhite: {
+    color: 'rgba(255,255,255,.62)',
+    margin: '0',
+    fontSize: '14px',
+    marginTop: '0',
+    marginBottom: '0',
+  },
+  cardTitleWhite: {
+    color: '#FFFFFF',
+    marginTop: '0px',
+    minHeight: 'auto',
+    fontWeight: '300',
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    marginBottom: '3px',
+    textDecoration: 'none',
+  },
+};
 
 const { REACT_APP_SERVER_URL } = process.env;
 
 class Stock extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        errors: {},
-      };
-    }
-  
-    render() {
-      return (
-        <div>
-            <TableStockSection />
-        </div>
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      errors: {},
+    };
   }
 
-  Stock.propTypes = {
-    classes: PropTypes.object.isRequired,
-    name: PropTypes.string,
-    email: PropTypes.string,
-  };
+  render() {
+    return (
+      <div>
+        <LoadPurchase />
+        <TableStockSection />
+      </div>
+    );
+  }
+}
 
-  export default withStyles(styles)(Stock);
-  
+Stock.propTypes = {
+  classes: PropTypes.object.isRequired,
+  name: PropTypes.string,
+  email: PropTypes.string,
+};
+
+export default withStyles(styles)(Stock);
