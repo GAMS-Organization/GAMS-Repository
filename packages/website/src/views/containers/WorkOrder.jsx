13118063b04b71a951dda45b16fa249e424ea032
javi <javisicardi94@gmail.com>
2020-09-08 18:39:43 -0300

    Creada rama

---
+++ packages/website/src/views/containers/WorkOrder.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+// @material-ui/core components
+import withStyles from '@material-ui/core/styles/withStyles';
+// core components
+
+import NewUserSection from '../sections/users/NewUserSection';
+import UsersTable from '../sections/users/UsersTableSection';
+
+const styles = {
+  cardCategoryWhite: {
+    color: 'rgba(255,255,255,.62)',
+    margin: '0',
+    fontSize: '14px',
+    marginTop: '0',
+    marginBottom: '0',
+  },
+  cardTitleWhite: {
+    color: '#FFFFFF',
+    marginTop: '0px',
+    minHeight: 'auto',
+    fontWeight: '300',
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    marginBottom: '3px',
+    textDecoration: 'none',
+  },
+};
+
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      errors: {},
+    };
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+Users.propTypes = {
+  classes: PropTypes.object.isRequired,
+  name: PropTypes.string,
+  email: PropTypes.string,
+};
+
+export default withStyles(styles)(Users);
