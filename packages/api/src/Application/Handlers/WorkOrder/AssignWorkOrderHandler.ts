0be9acfe8a894abc9f39c59fac1980a19b84252e
AleMinacori <alenemo4@hotmail.com>
2021-02-04 20:08:02 -0300

    modificando el proceso de crear mensajes

--- packages/api/src/Application/Handlers/WorkOrder/AssignWorkOrderHandler.ts
+++ packages/api/src/Application/Handlers/WorkOrder/AssignWorkOrderHandler.ts
@@ -7,25 +7,20 @@ import EntityNotFoundException from '../../Exceptions/EntityNotFoundException';
 import IUserRepository from '../../../Domain/Interfaces/IUserRepository';
 import WorkOrderService from '../../../Domain/Services/WorkOrderService';
 import { STATE } from '../../../API/Http/Enums/WorkOrder';
-import MailerService from '../../../Domain/Services/Mailer/MailerService';
-import { mailTitles } from '../../../Domain/Enums/MailTitlesAndMessages';
 
 @injectable()
 export default class AssignWorkOrderHandler {
   private workOrderRepository: IWorkOrderRepository;
   private userRepository: IUserRepository;
   private workOrderService: WorkOrderService;
-  private mailerService: MailerService;
   public constructor(
     @inject(INTERFACES.IWorkOrderRepository) workOrderRepository: IWorkOrderRepository,
     @inject(INTERFACES.IUserRepository) userRepository: IUserRepository,
     @inject(WorkOrderService) workOrderService: WorkOrderService,
-    @inject(MailerService) mailerService: MailerService,
   ) {
     this.workOrderRepository = workOrderRepository;
     this.userRepository = userRepository;
     this.workOrderService = workOrderService;
-    this.mailerService = mailerService;
   }
 
   public async execute(command: AssignWorkOrderCommand): Promise<WorkOrder> {
@@ -44,8 +39,6 @@ export default class AssignWorkOrderHandler {
       }
       workers.push(worker);
     }
-
-    await this.mailerService.sendEmail(mailTitles.workOrderAssigned, workOrder.getComment(), workers);
 
     return await this.workOrderService.updateWorkers(await this.workOrderRepository.persist(workOrder), workers);
   }
--- packages/api/src/Domain/Services/Mailer/MailerService.ts
+++ packages/api/src/Domain/Services/Mailer/MailerService.ts
@@ -1,6 +1,7 @@
 import UserService from '../UserService';
 import { inject, injectable } from 'inversify';
 import User from '../../Entities/User';
+import WorkOrder from '../../Entities/WorkOrder';
 
 @injectable()
 export default class MailerService {
@@ -12,8 +13,8 @@ export default class MailerService {
     this.sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   }
 
-  public async sendEmail(subject: string, message: string, user?: User[]) {
-    const recipient = await this.getRecipientMail(user);
+  public async sendEmail(subject: string, workOrder: WorkOrder, type: string, userId?: number[]) {
+    const recipient = await this.getRecipientMail(userId);
     //Recipient contiene el/los email del destinatario
     console.log(
       '----------------------------------------------------------------------',
@@ -24,7 +25,7 @@ export default class MailerService {
       to: process.env.RECIPIENT_EMAIL_TEST, // Cambiar por recipient
       from: process.env.EMAIL_SENDER, // Change to your verified sender
       subject: subject,
-      text: message,
+      text: this.getCustomMessage(type, workOrder),
     };
     this.sgMail
       .send(msg)
@@ -36,7 +37,14 @@ export default class MailerService {
       });
   }
 
-  private async getRecipientMail(user?: User[]) {
+  private async getRecipientMail(usersId?: number[]) {
+    const user: User[] = [];
+    if (usersId) {
+      for (const userId of usersId) {
+        const userFound: User = await this.userService.findOneById(userId);
+        user.push(userFound);
+      }
+    }
     const recipient: string[] = [];
     if (user) {
       user.forEach(user => {
@@ -49,5 +57,32 @@ export default class MailerService {
       });
     }
     return recipient;
+  }
+
+  private getCustomMessage(type: string, workOrder: WorkOrder): string {
+    let message: string = '';
+    switch (type) {
+      case 'newWorkOrder':
+        message = `El elemento ${workOrder
+          .getAsset()
+          .getElement()
+          .getName()} que se encuentra dentro del sector
+         ${workOrder
+           .getAsset()
+           .getSector()
+           .getName()}, área ${workOrder
+          .getAsset()
+          .getArea()
+          .getName()}, le ha sido
+          asignada una nueva órden de trabajo.
+           El usuario ${workOrder.getUser().getName()} ${workOrder.getUser().getSurname()} ha dejado el siguiente
+            comentario: ${workOrder.getComment()}. Código del activo correspondiente: ${workOrder
+          .getAsset()
+          .getCode()}`;
+        break;
+      default:
+        break;
+    }
+    return message;
   }
 }
