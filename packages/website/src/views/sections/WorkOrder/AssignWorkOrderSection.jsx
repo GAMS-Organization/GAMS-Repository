774cfc12b134ed1fe57b53901ea08c358fee3358
javi <javisicardi94@gmail.com>
2021-06-30 19:14:55 -0300

    arreglado mensaje en alert success

--- packages/website/src/views/sections/WorkOrder/AssignWorkOrderSection.jsx
+++ packages/website/src/views/sections/WorkOrder/AssignWorkOrderSection.jsx
@@ -107,7 +107,7 @@ class AssignWorkOrderSection extends React.Component {
           message={
             this.state.errors.code
               ? `Error ${this.state.errors.code}, ${this.state.errors.details}`
-              : 'Orden de trabajo tomada correctamente'
+              : 'Orden de trabajo asignada correctamente'
           }
           open={this.state.notification}
           closeNotification={this.closeNotification}
