285aa594c37c50ff0ce3a92f6e17301e0413c829
javi <javisicardi94@gmail.com>
2021-08-25 18:58:05 -0300

    eliminada importacion que no se usa

--- packages/website/src/views/sections/WorkOrder/DetailWorkOrderSection.jsx
+++ packages/website/src/views/sections/WorkOrder/DetailWorkOrderSection.jsx
@@ -12,13 +12,9 @@ import Button from '../../components/CustomButtons/Button.jsx';
 import Snackbar from '../../components/Snackbar/Snackbar';
 // @material-ui/icons components
 import AddAlert from '@material-ui/icons/AddAlert';
-
 import modalStyle from '../../../styles/jss/material-dashboard-react/modalStyle';
 import CardFooter from '../../components/Card/CardFooter';
-import CardBody from '../../components/Card/CardBody';
 import PropTypes from 'prop-types';
-import serviceWorkOrder from '../../../services/api/workOrder';
-import workOrder from '../../../services/api/workOrder';
 
 class DetailWorkOrderSection extends React.Component {
   constructor(props) {
