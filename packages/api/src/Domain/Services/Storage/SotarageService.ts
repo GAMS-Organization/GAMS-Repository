08c6510cc22d72c70d5d5b10e2f2bffa0d9b4123
AleMinacori <alenemo4@hotmail.com>
2021-10-20 16:10:37 -0300

    fix: localstorage backend

--- packages/api/src/Domain/Services/Storage/SotarageService.ts
+++ packages/api/src/Domain/Services/Storage/SotarageService.ts
@@ -2,13 +2,14 @@ import multer from 'multer';
 import { injectable } from 'inversify';
 import { RequestHandler } from 'express';
 import { IStorageService } from './IStorageService';
+import * as path from 'path';
 
 @injectable()
 class StorageService implements IStorageService {
-  public getConfig(path: string): RequestHandler {
+  public getConfig(name: string): RequestHandler {
     const storage = multer.diskStorage({
       destination: function(_req, _file, cb) {
-        cb(null, `/usr/src/service/packages/api/localStorage/${path}`);
+        cb(null, path.resolve(process.cwd(), `localStorage/${name}`));
       },
       filename: function(_req, file, cb) {
         cb(null, file.originalname);
